757d88ec507ae0c87b4ace3867be38441d50a1fa
Thomas Klemz <tjklemz@gmail.com>
2023-01-25 18:06:16 -0800

    types

--- src/workflow.cpp
+++ src/workflow.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char * argv[]) {
   std::cout << "---- Welcome ----\n\n";
 
-  for (size_t i{}; i < argc; ++i) {
+  for (int i{}; i < argc; ++i) {
     std::cout << "arg#" << argc << ": " << argv[i] << '\n';
   }
 }
